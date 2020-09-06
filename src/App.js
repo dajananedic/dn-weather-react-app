98789649757103a5370ffaee7cc02a36d380b6a4
Dajana Nedic <dajananedicuark@gmail.com>
2020-09-06 15:08:40 -0500

    Deleted Weather.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import Weather from "./Weather";
 import Search from "./Search";
 
 function App() {
--- src/Weather.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Weather() {
-  return <h2>Hello World!</h2>;
-}
