836cf6e8e04692267d46186b717408de350d3e76
Dajana Nedic <dajananedicuark@gmail.com>
2020-09-12 20:43:45 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Dajana's Weather App</h1>
-      <Search />
-      <footer>
-        This project was coded by Dajana Nedic{" "}
-        <span
-          href="https://github.com/dajananedic/dn-weather-react-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </span>
-      </footer>
+      <div className="container">
+        <button className="btn btn-primary">Hi There!</button>
+        <h1>Dajana's Weather App</h1>
+        <Search />
+        <footer>
+          This project was coded by Dajana Nedic{" "}
+          <span
+            href="https://github.com/dajananedic/dn-weather-react-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </span>
+        </footer>
+      </div>
     </div>
   );
 }
